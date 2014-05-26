2412764d26b61fd84f020b8579b4ed8fee0e4e24
mihirkelkar <kelkarmhr@gmail.com>
2014-05-26 14:02:28 -0700

    dont like writing commit comments

--- prob_five/prob_five.rb
+++ prob_five/prob_five.rb
@@ -6,18 +6,50 @@
 def getinteger()
 	puts "Enter an unsigned integer"
 	choice = gets().chomp
-	begin
-		choice = Integer(choice)
-		if choice < 0
-			choice = getinteger()	
-		end
-	rescue
-		puts "Not an integer, enter again"
-		choice = gets().chomp
+	if choice.include? "."
 		choice = getinteger()
+	else
+		begin
+			choice = Integer(choice)
+			if choice < 0
+				choice = getinteger()	
+			end
+		rescue
+			choice = getinteger()
+		end
 	end
 	return choice
 end
 
+def menu()
+	puts "Enter B for binary, O for Octal, H for hexadecimal\n"
+	menu_choice = gets.chomp().downcase
+	if ['b','o','h'].include? menu_choice
+		if menu_choice == 'b'
+			return 2
+		elsif menu_choice == 'o'
+			return 8
+		elsif menu_choice == 'h'
+			return 16
+		end
+	else
+		menu_choice = menu()
+	end
+	return menu_choice
+end
 
-puts getinteger()
+def convert_base(number, base)
+	#puts number
+	#puts base
+	base_list = []
+	until number == 0
+		base_list << (number % base).to_s
+		number = number / base
+	end
+	puts(base_list.join('').reverse)
+
+end
+
+number = getinteger()
+base =  menu()
+convert_base(number, base)
